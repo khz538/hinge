1132a4cf335b7abd1f59b0af6fa1ecf2d3da79cc
Kevin Zhang <zhang.kevin.hua@gmail.com>
2024-03-04 09:42:44 -0600

    tabs with icons rendered

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { colors } from "@/constants/colors";
+import { HingeIcon, StarIcon, HeartIcon, MessageIcon, PersonIcon } from "@/constants/icons";
 
 const TabLayout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarStyle: {
+          backgroundColor: colors.darkGrey1,
+        },
+        tabBarActiveTintColor: colors.white,
+        tabBarInactiveTintColor: colors.darkGrey2,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color }) => {
+          switch(route.name) {
+            case "index":
+              return <HingeIcon fill={color} />;
+            case "standouts":
+              return <StarIcon fill={color} />;
+            case "likes":
+              return <HeartIcon fill={color} />;
+            case "matches":
+              return <MessageIcon fill={color} />;
+            case "account":
+              return <PersonIcon fill={color} />;
+            default:
+              return null;
+          }
+        },
+      })}
+    >
         <Tabs.Screen name="index" />
         <Tabs.Screen name="standouts" />
         <Tabs.Screen name="likes" />
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,23 @@
-import { Slot } from "expo-router";
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { Slot, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { fonts } from '../constants/fonts';
+
+SplashScreen.preventAutoHideAsync();
 
 const AppLayout = () => {
+    const [fontsLoaded, fontError] = useFonts(fonts);
+
+    useEffect(() => {
+        if (fontsLoaded || fontError) {
+            // Hide the spash screen after the fonts have loaded or an error has occurred and the UI is ready
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontError]);
+
+    // Prevent rendering until the font has loaded or an error has occurred
+    if (!(fontsLoaded || fontError)) return null;
+
     return (
         <Slot />
     )
